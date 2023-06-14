809599c465e7eb39269d625d3c9641408ef1bf78
10000Kim <kckm317@gmail.com>
2023-06-15 07:05:39 +0900

    채팅프로그램

--- 02_WebSocket/app.js
+++ 02_WebSocket/app.js
@@ -1,30 +1,32 @@
-const { Socket } = require("dgram");
-const express = require("express")
+const { Socket } = require("dgram"); //dgram 모듈 Socket 클래스 가져오기 (사용X)
+const express = require("express")   //Express 모듈 가져옵니다.
 
-const http = require("http")
+const http = require("http")         //http 모듈 가져옵니다.
 
-const app = express();
-const path = require("path")
+const app = express();               //express 객체 생성
+const path = require("path")         //path 모듈 가져옵니다.
 
-const server = http.createServer(app)
+const server = http.createServer(app) //app을 이용해 http 서버 생성합니다.
 //express에 구현한 app이 http를 통해서 
 
-const socketIO = require("socket.io")
+const socketIO = require("socket.io") //socket.io 모듈 가져오기
 
-const moment = require("moment");
+const moment = require("moment");     //moment 모듈 가져오기
 const { monitorEventLoopDelay } = require("perf_hooks");
+//perf_hooks 모듈에서 monitorEventLoopDelay 함수를 가져옵니다.
 
-const io = socketIO(server);
-
+const io = socketIO(server); 
+// server를 이용하여 socket.io의 인스턴스(io)를 생성합니다.
 
 app.use(express.static(path.join(__dirname, "src")))
 
-let connectedUsers = 0; // 연결된 사용자 수
+let connectedUsers = 0; // 연결된 사용자 수를 저장하는 변수입니다.
 
 const PORT = process.env.PORT || 5000;
+// 환경 변수에서 포트 번호를 가져오거나 기본값으로 5000을 설정합니다.
 
 
-
+ // 클라이언트와의 소켓 연결 이벤트를 처리하는 콜백 함수입니다.
 io.on("connection", (socket) => {
     connectedUsers++; // 소켓 연결이 발생하면 연결된 사용자 수 증가
     io.emit("userCount", connectedUsers); // 연결된 사용자 수를 모든 클라이언트에게 전달
@@ -34,6 +36,7 @@ io.on("connection", (socket) => {
       io.emit("userCount", connectedUsers); // 연결된 사용자 수를 모든 클라이언트에게 전달
     })
   
+    // 채팅 메시지 이벤트 처리
     socket.on("chatting", (data) => {
       const { name, msg } = data;
       io.emit("chatting", {
