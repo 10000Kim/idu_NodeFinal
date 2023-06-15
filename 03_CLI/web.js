6ea79eb47ca16dd40fb30f6c3cda21bfe3356b5e
10000Kim <kckm317@gmail.com>
2023-06-15 09:30:13 +0900

    CLI 프로그램

--- 03_CLI/web.js
+++ 03_CLI/web.js
@@ -1,8 +1,8 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const readline = require('readline');
-const chalk = require('chalk')
-const fs = require('fs');
+const axios = require('axios');       // HTTP 요청을 보내기 위한 패키지
+const cheerio = require('cheerio');   // 웹 페이지 파싱을 위한 패키지
+const readline = require('readline'); // 사용자 입력을 받기 위한 패키지
+const chalk = require('chalk')        // 콘솔 출력 스타일링을 위한 패키지
+const fs = require('fs');             // 파일 시스템 접근을 위한 패키지
 
 // 사용자 입력을 받기 위한 인터페이스 생성
 const rl = readline.createInterface({
@@ -48,5 +48,5 @@ function scrapeWebPage(url) {
 // 사용자로부터 웹 페이지 URL 입력 받기
 rl.question('스크랩할 웹 페이지의 URL을 입력해주세요! ', url => {
   scrapeWebPage(url);
-  rl.close();
+  rl.close();  //입력 인터페이스 닫기
 });
